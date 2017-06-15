db991f337758f65364934b75965494e8ae00a581
Hans <wh1996fz@live.com>
2017-06-15 21:48:22 +0800

    Transfer to GitHub desktop

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 from Vector import *
-
+from Line import Line
 # a = Vector([3.039, 1.879])
 # b = Vector([0.825, 2.036])
 #
 # print(Vector.decompose(a, b)[0])
 
-c = Vector([8.462, 7.893, -8.187])
-d = Vector([6.984, -5.975, 4.778])
+line1 = Line(normal_vector=[4.046, 2.836], constant_term=1.21)
+line2 = Line(normal_vector=[10.115, 7.09], constant_term=3.025)
+print(line1.is_parallel(line2))
+print(line1.basepoint)
+print(line2.basepoint)
 
-print(c.cross_product(d))
+line3 = Line(normal_vector=[7.204, 3.182], constant_term=8.68)
+line4 = Line(normal_vector=[8,172, 4.114], constant_term=9.883)
+print(line3.is_parallel(line4))
 
-e = Vector([-8.987, -9.838, 5.031])
-f = Vector([-4.268, -1.861, -8.866])
-
-print(e.cross_product(f).get_magnitude())
-
-g = Vector([1.5, 9.547, 3.691])
-h= Vector([-6.007, 0.124, 5.772])
-
-print(g.cross_product(h).get_magnitude() / 2)
+line5 = Line(normal_vector=[1.182, 5.562], constant_term=6.744)
+line6 = Line(normal_vector=[1.773, 8.343], constant_term=9.525)
+print(line5.is_parallel(line6))
